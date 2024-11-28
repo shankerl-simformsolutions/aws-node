6b94c06a1c0f64a4301cb7191f03b50e9001f836
shankerl-simformsolutions <shanker.l@simformsolutions.com>
2024-11-28 12:39:36 +0530

    updated api added new route

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/api/get/users', (req,res) => {
     res.send({name : 'Dev', email: 'dev@gmail.com', 'role': 'Developer'});
 })
 
+app.get('/api/get/all', (req,res) => {
+    res.send({name : 'bhawani', email: 'bhawani@gmail.com', 'role': 'Senior Developer'});
+})
+
 app.listen(9000, (req,res) => {
     console.log('server running on port 9000')
 })
